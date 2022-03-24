7cd32d34729a5498b026e7e257b117d60cb357e2
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-03-24 20:04:17 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a[]=new int[6];
+        a[0]=10;
+        a[1]=569;
+        a[2]=640;
+        a[3]=180;
+        a[4]=650;
+        a[5]=50;
+        for(int i=0;i<a.length;i++)
+        System.out.println(a[i]);
+    }
+}
